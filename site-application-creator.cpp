730a6568e736733bf111ebf74fb63adaf6043975
William McKinnon <willpower3309@gmail.com>
2019-08-13 16:38:49 -0400

    Update site-application-creator.cpp

--- site-application-creator.cpp
+++ site-application-creator.cpp
@@ -17,19 +17,22 @@ int main() {
 	if (path[0] != 'v') {
 		cout << "Electron is not installed!";
 	}
+
 	// electron is installed, proceed with program
 	else {
 		cout << "Electron is installed!\n";
 		cout << "Enter the URL of the Website: ";
 
-		// write URL to file (which is then imported by the electron main.js)
+		// prompt user for URL
 		std::string siteUrl;
 		std::cin >> siteUrl;
+
+		// Verify siteUrl is a valid URL (https://stackoverflow.com/questions/38608116/how-to-check-a-specified-string-is-a-valid-url-or-not-using-c-code)
+
+		// write URL to file (which is then imported by the electron main.js)
 		std::ofstream out("electron-files/url.txt");
 		out << siteUrl;
 		out.close();
-
-		cout << "Converting " << siteUrl << " to Desktop App";
 
 		// run electron main.js
 		system("run-electron.bat");
