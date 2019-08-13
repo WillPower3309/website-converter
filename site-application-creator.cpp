6c8aa5acea03b1dd40ea50eeb861517a5eb4a689
William McKinnon <willpower3309@gmail.com>
2019-08-13 15:07:05 -0400

    now prints URL to text file

--- site-application-creator.cpp
+++ site-application-creator.cpp
@@ -3,29 +3,42 @@
 #include <iostream>
 #include <direct.h>
 #include <windows.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main() {
 	FILE* fp;
-	const int bufferSize = MAX_PATH;
-	char oldDir[bufferSize]; // store the current directory
 	char path[1035];
 
 	fp = _popen("electron --version", "r");
 
 	// wait until output is read
 	while (fgets(path, sizeof(path) - 1, fp) != NULL) {}
+	_pclose(fp);
 
 	// if electron is not installed, must first be installed (assumes node is installed)
 	if (path[0] != 'v') {
-		printf("Electron is not installed!\n");
+		cout << "Electron is not installed!";
 	}
 	// electron is installed, proceed with program
 	else {
-		printf("Electron is installed!\n");
+		cout << "Electron is installed!\n";
+		cout << "Enter the URL of the Website: ";
+
+		// write URL to file (which is then imported by the electron main.js)
+		std::string siteUrl;
+		std::cin >> siteUrl;
+		std::ofstream out("electron-files/url.txt");
+		out << siteUrl;
+		out.close();
+
+		cout << "Converting " << siteUrl << " to Desktop App";
+
+		// run electron main.js
 		system("run-electron.bat");
 	}
-
-	_pclose(fp);
 
 	return 0;
 }
