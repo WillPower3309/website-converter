70c01cb21e05b218b4207c9e490cac6df7dc82da
William McKinnon <willpower3309@gmail.com>
2019-08-13 14:25:48 -0400

    changed from c to c++

--- site-application-creator.cpp
+++ site-application-creator.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <direct.h>
+#include <windows.h>
 
 int main() {
 	FILE* fp;
+	const int bufferSize = MAX_PATH;
+	char oldDir[bufferSize]; // store the current directory
 	char path[1035];
 
 	fp = _popen("electron --version", "r");
@@ -17,9 +22,17 @@ int main() {
 	// electron is installed, proceed with program
 	else {
 		printf("Electron is installed!\n");
-	}
 
-	system("dir");
+		// get the current directory, and store it
+		//_getcwd(oldDir, sizeof(oldDir));
+		//_chdir("D:\Windows\Documents\GitHub\website-converter\Debug\electron-files");
+		//printf("%s\n", oldDir);
+		//system("dir");
+		//system("cd electron-files");
+		//system("electron .");
+		system("dir");
+		system("run-electron.bat");
+	}
 
 	_pclose(fp);
 
